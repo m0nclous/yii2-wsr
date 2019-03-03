b4e98da652fdfb6e28272305f39e8c6c0bc561ea
m0nclous <m0nclous.official@gmail.com>
2019-03-03 16:15:06 +0600

    Создана миграция таблицы тегов

---
+++ migrations/m190303_101301_create_tag_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tag}}`.
+ */
+class m190303_101301_create_tag_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%tag}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%tag}}');
+    }
+}
