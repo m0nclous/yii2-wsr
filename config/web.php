df621ee15c4ef216f62e9b03190eea0e555566b6
m0nclous <m0nclous.official@gmail.com>
2019-03-03 15:24:51 +0600

    Установлен cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'cr4he5yhv56y34y46u',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
