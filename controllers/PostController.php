5e7c8ded9f712296637735c2678f28539228fb2c
m0nclous <m0nclous.official@gmail.com>
2019-03-03 17:45:29 +0600

    Перезаписан метод создания постов

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -7,4 +7,18 @@ use yii\rest\ActiveController;
 class PostController extends ActiveController
 {
     public $modelClass = 'app\models\Post';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        unset($actions['create']);
+
+        return $actions;
+    }
+
+    public function actionCreate()
+    {
+
+    }
 }
